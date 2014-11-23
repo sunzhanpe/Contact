ca5a7ac548c460371d15006e39f66d7f19f6f90e
sunzhanpeng <sunzhanpe@gmail.com>
2014-11-23 22:38:47 +0800

    加User类

---
+++ Contact/src/com/example/contact/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.contact.entity;
+
+import android.R.integer;
+
+public class User
+{
+	public int _id;
+	
+	public int imageId;
+	
+	public String name;
+	
+	public String mobliePhone;
+	
+	public String officePhone;
+	
+	public String familyPhone;
+	
+	public String position;
+	
+	public String companyString;
+	
+	public String address;
+	
+	public String zipcode;
+	
+	public String email;
+	
+	public String otherContact;
+	
+	public String remark;
+	
+	
+	
+	
+	
+}
