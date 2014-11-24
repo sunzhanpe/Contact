525aac22d18986de27aa8d06c7090c1cc75e8753
sunzhanpeng <sunzhanpe@gmail.com>
2014-11-24 14:22:55 +0800

    添加主界面

---
+++ Contact/src/com/example/contact/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.contact;
+
+import com.example.content.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+}
